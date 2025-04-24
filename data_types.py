0a187f7c4d278ffd3f9bd3bc5a305ad98aabd815
aozoragh <satoshi.a9877@gmail.com>
2025-04-24 15:26:49 -0700

    feat: string type course

---
+++ data_types.py
@@ -0,0 +1,15 @@
+#String data type
+
+#literal assignment
+first = "Francesco"
+last = "Popoescu"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+#constructor function
+pizza = str("Pepperroni")
+print(type(pizza))
+print(type(pizza) == str)
+print(isinstance(pizza, str))
